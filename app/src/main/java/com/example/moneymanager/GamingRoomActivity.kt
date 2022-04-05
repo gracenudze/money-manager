2242abc0c6ccbf27bf2ef78ef09495b0fa371756
unknown <graceynud@gmail.com>
2022-04-05 22:59:14 +0000

    Making use of view binding

--- app/src/main/java/com/example/moneymanager/GamingRoomActivity.kt
+++ app/src/main/java/com/example/moneymanager/GamingRoomActivity.kt
@@ -2,11 +2,15 @@ package com.example.moneymanager
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.moneymanager.databinding.GamingRoomBinding
+import com.example.moneymanager.databinding.GamingRoomBindingBinding
 import com.example.moneymanager.model.Activity
 
+private lateinit var binding: GamingRoomBinding
 class GamingRoomActivity : AppCompatActivity() {
     private val activities = listOf(
         Activity(R.drawable.ic_shopping_cart, "Shopping", "Jan 24, 2022", "$-54"),
@@ -16,15 +20,17 @@ class GamingRoomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gaming_room)
+        binding = GamingRoomBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
 
         val activityRecycler: RecyclerView = findViewById(R.id.rvActivities)
 
         activityRecycler.adapter =  GamingRoomAdapter(activities)
-        val btnBack: ImageButton = findViewById(R.id.btnBack)
-        btnBack.setOnClickListener {
+
+        binding.btnBack.setOnClickListener {
             val intent = Intent(this@GamingRoomActivity, MainActivity::class.java)
-            startActivity(intent)
+            startActivity(intent) }
         }
     }
 }
